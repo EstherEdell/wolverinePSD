fff0deadc84f84d3a504e550e7a6a6c116f48bef
Esther Edell <esther.edell@gmail.com>
2018-04-15 16:20:50 -0400

    working on styles

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,6 +1,22 @@
 const app = {};
 
+// app.showWindow = function(){
+//     
+// }
+
+const label = document.querySelectorAll('label');
+const info = document.querySelectorAll('blurb');
 app.events = function () {
+    for(let i=0; i < label.length; i++){
+        label[i].addEventListener('click', function(){
+            blurb.classList.toggle('hide');
+        })
+    }
+    // document.querySelectorAll('photography').addEventListener('click', function(){
+    //     console.log('tst');
+    //     document.getElementById("blurb").classList.toggle("hide");
+    // })
+
 };
 
 app.init = function () {
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,8 +1,24 @@
-"use strict";
+'use strict';
 
 var app = {};
 
-app.events = function () {};
+// app.showWindow = function(){
+//     
+// }
+
+var label = document.querySelectorAll('label');
+var info = document.querySelectorAll('blurb');
+app.events = function () {
+    for (var i = 0; i < label.length; i++) {
+        label[i].addEventListener('click', function () {
+            blurb.classList.toggle('hide');
+        });
+    }
+    // document.querySelectorAll('photography').addEventListener('click', function(){
+    //     console.log('tst');
+    //     document.getElementById("blurb").classList.toggle("hide");
+    // })
+};
 
 app.init = function () {
     app.events();
